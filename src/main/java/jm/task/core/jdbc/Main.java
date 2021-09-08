5d8756c076ccf8dcb2998adbf6a065756609b151
Anton Garussky <www.snowfall@mail.ru>
2021-09-09 00:43:48 +0300

    на правильном ли я пути?

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,27 +1,46 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class Main {
     public static void main(String[] args) {
-        UserService userService = new UserServiceImpl();
 
-        userService.createUsersTable();
-        userService.saveUser("John", "Smith", (byte) 50);
-        userService.saveUser("Jane", "Smith", (byte) 45);
-        userService.saveUser("Steve", "Malkovich", (byte) 30);
-        userService.saveUser("Peter", "Pen", (byte) 15);
+        Session session = Util.getSessionFactory().getCurrentSession();
 
-        userService.getAllUsers();
+        session.beginTransaction();
 
-        userService.removeUserById(2L);
 
-        userService.cleanUsersTable();
+        session.getTransaction().commit();
 
-        userService.getAllUsers();
 
-        userService.dropUsersTable();
+
+//        UserService userService = new UserServiceImpl();
+//
+//        userService.createUsersTable();
+//        userService.saveUser("John", "Smith", (byte) 50);
+//        userService.saveUser("Jane", "Smith", (byte) 45);
+//        userService.saveUser("Steve", "Malkovich", (byte) 30);
+//        userService.saveUser("Peter", "Pen", (byte) 15);
+//
+//        userService.getAllUsers();
+//
+//        userService.removeUserById(2L);
+//
+//        userService.cleanUsersTable();
+//
+//        userService.getAllUsers();
+//
+//        userService.dropUsersTable();
+
+
+
 
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,19 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    UserDao userDao = new UserDaoJDBCImpl();
+    Session session = Util.getSessionFactory().getCurrentSession();
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,31 +21,39 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        userDao.createUsersTable();
     }
 
     @Override
     public void dropUsersTable() {
-
+        userDao.dropUsersTable();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction transaction = session.beginTransaction(); //TODO по такой аналогиий переопределять остальные методы?
+        User user  = new User(name, lastName, age);
+        session.persist(user);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void removeUserById(long id) {
-
+        userDao.removeUserById(id);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = userDao.getAllUsers();
+        for (User user: users) {
+            System.out.println(user);
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        userDao.cleanUsersTable();
     }
 }
